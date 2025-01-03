e4c7d39204165bb5bc7743d59c5cf3f358116cfa
Toni Müller <toni.mueller.web@mailbox.org>
2025-01-03 15:34:36 +0100

    fix: GH-26 Add content to rss feeds (#27)
    * fix: GH-26 Add content to rss feeds
    
    * fix: GH-26 Add separate title and desc for notes
    
    * fix: GH-26 make `markdown` post a draft again
    
    * fix: GH-26 Use fallback for empty body

--- src/pages/notes/rss.xml.ts
+++ src/pages/notes/rss.xml.ts
@@ -1,18 +1,26 @@
 import { getCollection } from "astro:content";
 import { siteConfig } from "@/site.config";
 import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export const GET = async () => {
 	const notes = await getCollection("note");
 
 	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
+		title: "ToniTypes Notes",
+		description: "Collection of notes, not long enough for whole article",
 		site: import.meta.env.SITE,
 		items: notes.map((note) => ({
+			author: siteConfig.author,
 			title: note.data.title,
 			pubDate: note.data.publishDate,
 			link: `notes/${note.id}/`,
+			content: sanitizeHtml(parser.render(note.body ?? ""), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			}),
 		})),
 	});
 };
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import { getAllPosts } from "@/data/post";
 import { siteConfig } from "@/site.config";
 import rss from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export const GET = async () => {
 	const posts = await getAllPosts();
@@ -10,10 +14,14 @@ export const GET = async () => {
 		description: siteConfig.description,
 		site: import.meta.env.SITE,
 		items: posts.map((post) => ({
+			author: siteConfig.author,
 			title: post.data.title,
 			description: post.data.description,
 			pubDate: post.data.publishDate,
 			link: `posts/${post.id}/`,
+			content: sanitizeHtml(parser.render(post.body ?? ""), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+			}),
 		})),
 	});
 };
